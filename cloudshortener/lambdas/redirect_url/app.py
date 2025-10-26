75cbfe8e02e69fe562fe80e63133d521ab1697ae
Petar Nikolov <penikolov23@gmail.com>
2025-10-26 17:19:53 +0200

    [RDIR] Add successful redirect response and error handling
    The URL redirection lambda currently supports the following status
    codes:
    - 302: redirects the user to found long URL (associated with the
    short URL)
    - 400: Bad user request (missing 'shortcode' in path)
    - 500: Internal server error when config is missing

--- cloudshortener/lambdas/redirect_url/app.py
+++ cloudshortener/lambdas/redirect_url/app.py
@@ -1,13 +1,11 @@
 import json
+from typing import Dict, Any
 
-import redis
-
-from cloudshortener.models import ShortURLModel
 from cloudshortener.dao.redis import ShortURLRedisDAO
-from cloudshortener.utils import load_config, app_env
+from cloudshortener.utils import load_config, app_env, app_name
 
 
-def lambda_handler(event, context):
+def lambda_handler(event: Dict[str, Any], context: Any) -> Dict[str, Any]:
     """Sample pure Lambda function
 
     Parameters
@@ -36,40 +34,46 @@ def lambda_handler(event, context):
         }
     }
 
-{"APP_ENV":"local","APP_NAME":"cloudshortener","PROJECT_ROOT":"/var/task/"}
+    {"APP_ENV":"local","APP_NAME":"cloudshortener","PROJECT_ROOT":"/var/task/"}
     """
 
+    # 0- Get application's config
+    try:
+        app_config = load_config('redirect_url')
+    except FileNotFoundError:
+        return {
+            'statusCode': 500,
+            'body': json.dumps({
+                'message': "Internal Server Error",
+            }),
+        }
+
+    # 1- Extract shortcode from request's path
     shortcode = str(event.get('pathParameters', {}).get('shortcode'))
-    # TODO: if shortcode is none => return 400 error
+    if shortcode is None:
+        return {
+            'statusCode': 400,
+            'body': json.dumps({
+                'message': "Bad Request (missing 'shortcode' in path)",
+            }),
+        }
 
-    # 1- Create DAO class to access short URL records
-    """
-    redis_config = {
-        'redis_host': 'redis',
-        'redis_port': 6379,
-        'redis_db': 0,
-        'redis_decode_responses': True
-    }
-    """
-    app_config = load_config('redirect_url')
+    # 2- Create DAO class to access short URL records
     redis_config = {f'redis_{k}': v for k, v in app_config['redis'].items()}
-    short_url_dao = ShortURLRedisDAO(**redis_config, prefix='cloudshortener:local')
+    # TODO: move the prefix creation to a helper function?
+    # TODO: add error handling for Redis connection error
+    prefix = None if app_name() is None else f'{app_name()}:{app_env()}'
+    short_url_dao = ShortURLRedisDAO(**redis_config, prefix=prefix)
 
-    # 2- Get short_url record from database
+    # 3- Get short_url record from database
+    # TODO: add error handling for invalid short codes
     short_url = short_url_dao.get(short_code=shortcode)
+    original_url = short_url.original_url
 
     return {
         'statusCode': 302,
-        'body': json.dumps({
-            'message': "Hello from Redirect URL Lambda!",
-            'shortcode': short_url.short_code,
-            'original_url': short_url.original_url,
-            'expires_at': short_url.expires_at.isoformat(),
-            'ttl': short_url.expires_at.timestamp(),
-            'short_url': str(short_url_dao),
-            'app_config': app_config,
-            'app_env': app_env(),
-        }),
+        'headers': {
+            'Location': original_url,
+        },
+        'body': ''  # no body needed for redirects
     }
-
-
